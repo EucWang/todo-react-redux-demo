e9ece68fc732f0aa8daba4c2074b1b6e17b892cf
王新年 <wangxn@feezu.cn>
2018-06-26 14:08:17 +0800

    完善Readme

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import TodoApp from './TodoApp.js';
 import registerServiceWorker from './registerServiceWorker';
-import store from './Store.js'
-import {Provider} from 'react-redux'
+import store from './Store.js';
+import {Provider} from 'react-redux';
 
 ReactDOM.render((<Provider store={store}>
         <TodoApp />
